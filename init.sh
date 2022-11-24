64e085a40fc43ab3db2757b709cc1c477b02b607
Object <xiaowei@chinabaiker.com>
2022-11-24 14:33:04 +0800

    update

--- init.sh
+++ init.sh
@@ -7,6 +7,13 @@ yum -y groupinstall "Development tools"
 yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel
 yum install libffi-devel -y
 systemctl stop firewalld.service
+yum install -y squid
+yum install -y httpd
+yum install -y lsof
+rm -rf /etc/squid/squid.conf
+cp squid.conf /etc/squid/squid.conf
+squid stop
+squid start
 # sed -i "s/Allow 127.0.0.1/#Allow 127.0.0.1/g" /etc/tinyproxy/tinyproxy.conf
 # service tinyproxy restart
 cd ~
@@ -19,13 +26,7 @@ make && make install
 ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3.7
 ln -s /usr/local/python3/bin/pip3 /usr/local/bin/pip3.7
 pip3.7 install redis tornado requests environs
-yum install -y squid
-yum install -y httpd
-yum install -y lsof
-rm -rf /etc/squid/squid.conf
-cp squid.conf /etc/squid/squid.conf
-squid stop
-squid start
+
 # systemctl stop tinyproxy.service
 python3.7 -V
 pip3.7 -V
